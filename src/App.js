27790962f1f7d6a3c3ede1af375c5e37fc099693
id14mhn@cs.umu.se <Majsan92>
2020-02-19 22:10:05 +0100

    test

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo-op" alt="logo" />
         <p>
           Jag älskar Beppe
         </p>

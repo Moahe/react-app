9d1bf12e2956bcfc72433b463f4a0183e6bcb266
id14mhn@cs.umu.se <Majsan92>
2020-02-19 21:27:59 +0100

    test

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Jag älskar Beppe
         </p>
         <a
           className="App-link"
